aa8e1d50bae38c4e6691c16b38a75ad39a3124a2
kaladin <mathias.mohn.morech@proton.me>
2025-07-05 19:10:44 +0200

    Working on better nvim setup.
    Need to make it possible to use f for nvim leap plugin. Its set to s
    right now, and thats causing some conflicts with surround (which is
    something I definetly should learn to), need to learn more about how the
    keymaps are handeled.

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -292,6 +292,19 @@ require('lazy').setup({
       --vim.keymap.del({ 'n' }, 'f')
     end,
   },
+  {
+    'ggandor/flit.nvim',
+    enabled = true,
+    keys = function()
+      ---@type LazyKeysSpec[]
+      local ret = {}
+      for _, key in ipairs { 'f', 'F', 't', 'T' } do
+        ret[#ret + 1] = { key, mode = { 'n', 'x', 'o' } }
+      end
+      return ret
+    end,
+    opts = { labeled_modes = 'nx' },
+  },
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
