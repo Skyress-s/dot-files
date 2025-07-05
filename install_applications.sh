daf170570382e409b5c2c1e8b50937fae96ec395
kaladin <mathias.mohn.morech@proton.me>
2025-07-05 18:55:58 +0200

    adding a very basic shell script to install some programs I usually use!

---
+++ install_applications.sh
@@ -0,0 +1,22 @@
+echo Installing git...
+yay -S git-git 
+echo Done Installing git!
+
+# Install kitty terminal? or whatever terminal im using hehe
+
+echo Installing lazygit...
+yay -S lazygit
+echo Done Installing lazygit!
+
+echo Installing nvim...
+yay -S nvim 
+echo Done Installing nvim!
+
+echo Installing odin-lang...
+yay -S odin-git 
+echo Done Installing odin-lang!
+
+echo Installing yazi...
+yay -S yazi-git 
+echo Done Installing yazi!
+
