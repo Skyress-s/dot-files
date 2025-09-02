bfe5f8d1ba026c6357ee04bb2b8968783da9e999
Mathias Mohn Mørch <62551684+Skyress-s@users.noreply.github.com>
2025-09-02 11:18:07 +0200

    Update init.lua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -26,27 +26,50 @@ vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
 vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 vim.keymap.set('n', '<leader><F9>', ':term odin run src/<CR>')
 -- vim.keymap.set('n', 'nn', 'vim.diagnostic.open_float()')
+--
+vim.api.nvim_set_keymap('n', 'd', '"dd', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', 'x', '"_x', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', 'dd', '"ddd', {noremap = true, silent = true})
+--
+vim.keymap.set('n', '<leader>vr', ':Pick visit_paths<CR>')
+vim.keymap.set('n', '<leader>vl', ':Pick visit_labels<CR>')
+vim.keymap.set('n', '<leader>sr', ':lua MiniSessions.select("read")<CR>')
+
 
 vim.pack.add {
-  -- { src = "https://github.com/vague2k/vague.nvim" },
-  { src = 'https://github.com/folke/tokyonight.nvim' },
-  -- { src = "https://github.com/catppuccin/nvim" },
-  { src = 'https://github.com/folke/which-key.nvim' },
-  { src = 'https://github.com/rebelot/kanagawa.nvim' },
-  { src = 'https://github.com/stevearc/oil.nvim' },
-  { src = 'https://github.com/echasnovski/mini.pick' },
-  { src = 'https://github.com/echasnovski/mini.extra' },
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
-  { src = 'https://github.com/neovim/nvim-lspconfig' },
-  { src = 'https://github.com/chomosuke/typst-preview.nvim' },
-  { src = 'https://github.com/mason-org/mason.nvim' },
-  { src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
-  { src = 'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim' },
-  { src = 'https://github.com/Saghen/blink.cmp' },
-  { src = 'https://github.com/ggandor/leap.nvim' },
-  { src = 'https://github.com/nvim-lualine/lualine.nvim' },
-  { src = 'https://github.com/nvim-treesitter/nvim-treesitter-context' },
+	-- { src = "https://github.com/vague2k/vague.nvim" },
+	{ src = 'https://github.com/folke/tokyonight.nvim' },
+	-- { src = "https://github.com/catppuccin/nvim" },
+	{ src = 'https://github.com/folke/which-key.nvim' },
+	{ src = 'https://github.com/rebelot/kanagawa.nvim' },
+	{ src = 'https://github.com/stevearc/oil.nvim' },
+	{ src = 'https://github.com/echasnovski/mini.pick' },
+	{ src = 'https://github.com/echasnovski/mini.extra' },
+	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter' },
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },
+	{ src = 'https://github.com/chomosuke/typst-preview.nvim' },
+	{ src = 'https://github.com/mason-org/mason.nvim' },
+	{ src = 'https://github.com/mason-org/mason-lspconfig.nvim' },
+	{ src = 'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim' },
+	{ src = 'https://github.com/Saghen/blink.cmp' },
+	{ src = 'https://github.com/ggandor/leap.nvim' },
+	{ src = 'https://github.com/nvim-lualine/lualine.nvim' },
+	{ src = 'https://github.com/nvim-treesitter/nvim-treesitter-context' },
+	{ src = 'https://github.com/nvim-mini/mini.sessions' },
+	{ src = 'https://github.com/nvim-mini/mini.visits' },
 }
+require('mini.sessions').setup({})
+require('mini.visits').setup({})
+require('mini.pick').setup({})
+
+-- Override nvim normal ui.select function to use Mini.Pick's ui_select instead!
+-- This means that other plugins that uses vim.ui.select will now appear here instead
+vim.ui.select = MiniPick.ui_select
+-- Customize with fourth argument inside a function wrapper
+vim.ui.select = function(items, opts, on_choice)
+	local start_opts = { window = { config = { width = vim.o.columns } } }
+	return MiniPick.ui_select(items, opts, on_choice, start_opts)
+end
 
 vim.diagnostic.config({
 	update_in_insert = true,
@@ -54,14 +77,14 @@ vim.diagnostic.config({
 })
 -- Autocommand to trigger diagnostics update on text change
 vim.api.nvim_create_autocmd({ "TextChanged", "InsertLeave" }, {
-  callback = function()
-    vim.diagnostic.setloclist({ open = false }) -- optional: update loclist without opening
-  end,
+	callback = function()
+		vim.diagnostic.setloclist({ open = false }) -- optional: update loclist without opening
+	end,
 })
 
-require('treesitter-context').setup{}
+require('treesitter-context').setup {}
 
-require('which-key').setup{
+require('which-key').setup {
 }
 
 require('lualine').setup {}
@@ -71,43 +94,43 @@ require('mini.extra').setup {}
 require('leap').set_default_mappings() -- Super based
 
 require('blink.cmp').setup {
-  -- fuzzy = { implementation = "prefer_rust_with_warning" },
-  fuzzy = { implementation = 'lua' }, -- This should probably use rust instead. But lua is easier to compile. Will probably be slightly slower, but works fine for now!
-  -- build = 'cargo build --release',
-  signature = { enabled = true }, -- TODO: Why is this not working currently?
-  -- version = '1.*'
+	-- fuzzy = { implementation = "prefer_rust_with_warning" },
+	fuzzy = { implementation = 'lua' }, -- This should probably use rust instead. But lua is easier to compile. Will probably be slightly slower, but works fine for now!
+	-- build = 'cargo build --release',
+	signature = { enabled = true },  -- TODO: Why is this not working currently?
+	-- version = '1.*'
 }
 
 require('mason').setup()
 require('mason-lspconfig').setup()
 require('mason-tool-installer').setup {
-  ensure_installed = {
-    'lua_ls',
-    'stylua',
-    'ols',
-  },
+	ensure_installed = {
+		'lua_ls',
+		'stylua',
+		'ols',
+	},
 }
 
 vim.lsp.config('lua_ls', {
-  settings = {
-    Lua = {
-      runtime = {
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        globals = {
-          'vim',
-          'require',
-        },
-      },
-      workspace = {
-        library = vim.api.nvim_get_runtime_file('', true),
-      },
-      telemetry = {
-        enable = false,
-      },
-    },
-  },
+	settings = {
+		Lua = {
+			runtime = {
+				version = 'LuaJIT',
+			},
+			diagnostics = {
+				globals = {
+					'vim',
+					'require',
+				},
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file('', true),
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 })
 
 --
@@ -121,10 +144,9 @@ vim.lsp.config('lua_ls', {
 --})
 --vim.cmd("set completeopt+=noselect")
 
-require('mini.pick').setup()
 require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'diff', 'svelte', 'typescript', 'javascript', 'odin', 'cpp' },
-  highlight = { enable = true },
+	ensure_installed = { 'diff', 'svelte', 'typescript', 'javascript', 'odin', 'cpp' },
+	highlight = { enable = true },
 }
 require('oil').setup()
 
@@ -144,9 +166,9 @@ vim.keymap.set('n', 'grd', vim.lsp.buf.definition)
 vim.lsp.enable { 'lua_ls', 'biome', 'tinymist', 'emmetls', 'ols', 'clangd' }
 
 vim.lsp.config('ols', {
-  init_options = {
-    enable_references = true,
-  },
+	init_options = {
+		enable_references = true,
+	},
 })
 
 -- require "vague".setup({ transparent = true })
@@ -161,5 +183,3 @@ vim.cmd("colorscheme tokyonight-night") -- night moon storm day
 
 -- require('kanagawa').setup {}
 -- vim.cmd 'colorscheme kanagawa-wave' -- wave, dragon, lotus
-
-
